084a72a8c538d9b054f6a32d42dc56919584a6f7
EFP18 <ester.p18@hotmail.com>
2023-03-22 18:46:20 -0500

    added color classes and got event listener to work

--- script.js
+++ script.js
@@ -12,79 +12,6 @@ function displayTime(){
 setInterval(displayTime, 1000);
 
 
-$(function () {
-
-  var businessHoursInt = [9, 10, 11, 12, 13, 14, 15, 16, 17]
-  var businessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
-
-  for (var i=0; i<businessHours.length; i++){
-    // for each element of the array, so each business hour
-    // create a div which is my row, with 3 columns, one for the time, one for the description, one for the save button
-    // add the classes for each, and append to the container 
-    var container = $("#container");
-
-    var rowSchedule = $("<div>");
-    rowSchedule.addClass("row time-block")
-    container.append(rowSchedule)
-    // loop through the array and add an id of hour[i] to every element
-    $(rowSchedule).attr("id", "hour" + "-" + ([i+9])); 
-    // i+9 gives the ID an "index" that equals the hour shown on the schedule
-    // this will help with the comparison of the current hour
-
-
-    var timeCol =$("<div>");
-    timeCol.text(businessHours[i])
-    timeCol.addClass("col-2 col-md-1 hour text-center py-3")
-    rowSchedule.append(timeCol);
-
-    var descriptionCol = $("<textarea>");
-    descriptionCol.addClass("col-8 col-md-10 description", rows="3");
-    rowSchedule.append(descriptionCol);
-
-    var saveBtn = $("<button>");
-    saveBtn.addClass("btn saveBtn col-2 col-md-1");
-    saveBtn.attr("aria-label", "save")
-    rowSchedule.append(saveBtn);
-    $(saveBtn).attr("id", "buttonTime" + "-" + ([i+9])); 
-
-
-    var iInBtn = $("<i>");
-    iInBtn.addClass("fas fa-save");
-    iInBtn.attr("aria-hidden", "true")
-    saveBtn.append(iInBtn)
-
-
-  }
-gg
-  // function compareHour(){
-    
-    var currentHour = dayjs().format("h");
-    var currentHourInt = parseInt(currentHour);
-    console.log(currentHourInt)
-
-    for (var i=0; i<businessHoursInt.length; i++){
-      // if the [i] of id hour is the same as the index of businesshours 
-      if (currentHourInt === businessHoursInt[i]) {
-        rowSchedule.addClass("present");
-        console.log("present")
-
-      }
-      else if (currentHourInt < businessHoursInt[i]) {
-        rowSchedule.addClass("future");
-        console.log("future")
-
-      }
-      else if (currentHourInt > businessHoursInt[i]) {
-        rowSchedule.addClass("past");
-        console.log("past")
-
-
-      }
-      
-
-
-    }
-
 
   // }
 
@@ -111,24 +38,114 @@ function saveTasks(){
 
 function renderTasks(){
   // Use JSON.parse() to convert text to JavaScript object
-var savedTasks = JSON.parse(localStorage.getItem("tasksList"));
+  var hoursTextObj = JSON.parse(localStorage.getItem("hoursTextObj")) || {
+    9: "", 
+    10: "",
+    11: "", 
+    12: "", 
+    13: "", 
+    14: "", 
+    15: "", 
+    16: "", 
+    17: ""
+  };
+
+  var businessHoursInt = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+  var businessHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
+
+  var currentHour = dayjs().hour();
+  var currentHourInt = parseInt(currentHour);
+
+
+  for (var i=0; i<businessHours.length; i++){
+    // for each element of the array, so each business hour
+    // create a div which is my row, with 3 columns, one for the time, one for the description, one for the save button
+    // add the classes for each, and append to the container 
+    var container = $("#container");
+
+    var rowSchedule = $("<div>");
+    rowSchedule.addClass("row time-block")
+    container.append(rowSchedule)
+    // loop through the array and add an id of hour[i] to every element
+    $(rowSchedule).attr("id", i+9); 
+    // i+9 gives the ID an "index" that equals the hour shown on the schedule
+    // this will help with the comparison of the current hour
+
+
+    var timeCol =$("<div>");
+    timeCol.text(businessHours[i])
+    timeCol.addClass("col-2 col-md-1 hour text-center py-3")
+    rowSchedule.append(timeCol);
+
+    var descriptionCol = $("<textarea>");
+    descriptionCol.addClass("col-8 col-md-10 description", rows="3");
+
+    descriptionCol.text(hoursTextObj[i + 9]);
+    rowSchedule.append(descriptionCol);
+
+    var saveBtn = $("<button>");
+    saveBtn.addClass("btn saveBtn col-2 col-md-1");
+    saveBtn.attr("aria-label", "save")
+    rowSchedule.append(saveBtn);
+    $(saveBtn).attr("id", "buttonTime" + "-" + ([i+9])); 
+
+
+    var iInBtn = $("<i>");
+    iInBtn.addClass("fas fa-save");
+    iInBtn.attr("aria-hidden", "true")
+    saveBtn.append(iInBtn)
+
+    console.log("currentHourInt", currentHourInt, typeof currentHourInt);
+    console.log("businessHoursInt[i]", businessHoursInt[i], typeof businessHoursInt[i])
+
+    if (currentHourInt === businessHoursInt[i]) {
+      rowSchedule.addClass("present");
+  
+    }
+    else if (currentHourInt < businessHoursInt[i]) {
+      rowSchedule.addClass("future");
+  
+    }
+    else if (currentHourInt > businessHoursInt[i]) {
+      rowSchedule.addClass("past");
+  
+  
+    }
+  }
+    
 }
 
 
-$("#buttonTime-9, #buttonTime-10, #buttonTime-11, #buttonTime-12, #buttonTime-13, #buttonTime-14, #buttonTime-15, #buttonTime-16, #buttonTime-17").on("click", function(event){
+$("#container").on("click", ".saveBtn", function(event){
   event.preventDefault();
-  saveTasks();
-  renderTasks();
 
+  var textValue = $(this).siblings(".description").val()
+  var hourId = $(this).parent().attr("id");
+
+  var hoursTextObj = JSON.parse(localStorage.getItem("hoursTextObj")) || {
+    9: "", 
+    10: "",
+    11: "", 
+    12: "", 
+    13: "", 
+    14: "", 
+    15: "", 
+    16: "", 
+    17: ""
+  };
+
+  hoursTextObj[hourId] = textValue;
+
+  localStorage.setItem("hoursTextObj", JSON.stringify(hoursTextObj));
+  renderTasks();
 })
 
 // depending on which button was saved, save in that particular hour on local storage
 
 
+renderTasks();
 
 
-
-});
 
 
 
